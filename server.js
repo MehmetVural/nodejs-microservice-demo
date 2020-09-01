c8112877b9cdc5866e1aa538dcce24526be2dcfb
Mehmet Vural <mehmetalivural@yahoo.com>
2020-09-01 14:06:30 -0700

    re-building

--- server.js
+++ server.js
@@ -119,6 +119,12 @@ initDb(function(err){
   console.log('Error connecting to Mongo. Message:\n'+err);
 });
 
+const testController = require('./controller/test-controller.js');
+app.use('/test', testController);
+
+const keycloak = require('./config/keycloak-config.js').initKeycloak();
+app.use(keycloak.middleware());
+
 app.listen(port, ip);
 console.log('Server running on http://%s:%s', ip, port);
 
