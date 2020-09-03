ae045188ace4124ae37189352602141bccb6e288
Mehmet Vural <mehmetalivural@yahoo.com>
2020-09-02 23:10:26 -0700

    databasse

--- src/db/index.js
+++ src/db/index.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
 //const environment = require('../config/environment');
-
-mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true });
+mongoDB_URL                 = process.env.MONGODB_URL;
+mongoDB_database_user       = process.env.MONGODB_DATABASE-USER;
+mongoDB_database_password   = process.env.MONGODB_DATABASE_PASSWORD
+mongoDB_database_name       = process.env.MONGODB_DATABASE_NAME
+const mongourl = "mongodb://" + mongoDB_database_user + "@" + mongoDB_database_password + mongoDB_URL + "/" + mongoDB_database_name;
+mongoose.connect(mongourl, { useNewUrlParser: true });
 //mongoose.connect('mongodb+srv://localhost:27017/test', { useNewUrlParser: true });
 //var db = mongoose.connection;
 
