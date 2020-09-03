b32b035071bf511231f053895ecf15053bcb12bf
Mehmet Vural <mehmetalivural@yahoo.com>
2020-09-03 12:30:18 -0700

    db test

--- src/server.js
+++ src/server.js
@@ -69,9 +69,27 @@ app.get('/demo/admin', keycloak.protect('realm:admin'), function (req, res) {
 //  res.send('Not found!');
 //});
 */
+// TEST DATABASE CONNECTION 
+const mongoose = require('mongoose');
+//const environment = require('../config/environment');
+mongoDB_URL                 = "172.30.229.9:27017";
+mongoDB_database_user       = process.env.MONGODB_DATABASE_USER;
+mongoDB_database_password   = process.env.MONGODB_DATABASE_PASSWORD
+mongoDB_database_name       = process.env.MONGODB_DATABASE_NAME
+const mongourl = "mongodb://" + mongoDB_database_user + ":" + mongoDB_database_password +"@" + mongoDB_URL + "/" + mongoDB_database_name;
+
+//mongoose.connect(mongourl, { useNewUrlParser: true });
+mongoose.connect(mongourl, { useUnifiedTopology: true });
+
+//mongoose.connect('mongodb+srv://localhost:27017/test', { useNewUrlParser: true });
+//var db = mongoose.connection;
+
+mongoose.Promise = global.Promise;
+//TEST
+
 
 app.get('/', function (req, res) {
-  res.send(process.env.DATABASE_SERVICE_NAME);
+  res.send("Server is running");
 });
 
 //app.listen(port, ip);
