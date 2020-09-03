44aab89b6621269d2f41bf4da7e068f29adc391a
Mehmet Vural <mehmetalivural@yahoo.com>
2020-09-03 12:42:43 -0700

    test

--- src/db/index.js
+++ src/db/index.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 //const environment = require('../config/environment');
-mongoDB_URL                 = process.env.MONGODB_URL;
-mongoDB_database_user       = process.env.MONGODB_DATABASE-USER;
-mongoDB_database_password   = process.env.MONGODB_DATABASE_PASSWORD
-mongoDB_database_name       = process.env.MONGODB_DATABASE_NAME
+mongoDB_URL                 = "172.30.229.9:27017";
+mongoDB_database_user       = process.env.MONGODB_USER;
+mongoDB_database_password   = process.env.MONGODB_PASSWORD
+mongoDB_database_name       = process.env.MONGODB_DATABASE
 const mongourl = "mongodb://" + mongoDB_database_user + ":" + mongoDB_database_password +"@" + mongoDB_URL + "/" + mongoDB_database_name;
 console.log(mongourl);
 //mongoose.connect(mongourl, { useNewUrlParser: true });
--- src/server.js
+++ src/server.js
@@ -50,7 +50,7 @@ const router = express.Router();
 
 // APIS
 // keycloak.protect('realm:admin'),
-//app.use("/workbc/api/v1", require("./routes"));
+app.use("/workbc/api/v1", require("./routes"));
 
 /* Samples 
 app.get('/demo/public', function (req, res) {
@@ -69,23 +69,6 @@ app.get('/demo/admin', keycloak.protect('realm:admin'), function (req, res) {
 //  res.send('Not found!');
 //});
 */
-// TEST DATABASE CONNECTION 
-const mongoose = require('mongoose');
-//const environment = require('../config/environment');
-mongoDB_URL                 = "172.30.229.9:27017";
-mongoDB_database_user       = process.env.MONGODB_USER;
-mongoDB_database_password   = process.env.MONGODB_PASSWORD
-mongoDB_database_name       = process.env.MONGODB_DATABASE
-const mongourl = "mongodb://" + mongoDB_database_user + ":" + mongoDB_database_password +"@" + mongoDB_URL + "/" + mongoDB_database_name;
-
-//mongoose.connect(mongourl, { useNewUrlParser: true });
-mongoose.connect(mongourl, { useUnifiedTopology: true });
-
-//mongoose.connect('mongodb+srv://localhost:27017/test', { useNewUrlParser: true });
-//var db = mongoose.connection;
-
-mongoose.Promise = global.Promise;
-//TEST
 
 
 app.get('/', function (req, res) {
