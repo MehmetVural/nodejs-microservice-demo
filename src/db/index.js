2c06a8f204a5b91c56f767322c621023c7c41bc9
Mehmet Vural <mehmetalivural@yahoo.com>
2020-09-02 23:49:36 -0700

    test

--- src/db/index.js
+++ src/db/index.js
@@ -4,8 +4,11 @@ mongoDB_URL                 = process.env.MONGODB_URL;
 mongoDB_database_user       = process.env.MONGODB_DATABASE-USER;
 mongoDB_database_password   = process.env.MONGODB_DATABASE_PASSWORD
 mongoDB_database_name       = process.env.MONGODB_DATABASE_NAME
-const mongourl = "mongodb://" + mongoDB_database_user + "@" + mongoDB_database_password + mongoDB_URL + "/" + mongoDB_database_name;
-mongoose.connect(mongourl, { useNewUrlParser: true });
+const mongourl = "mongodb://" + mongoDB_database_user + ":" + mongoDB_database_password +"@" + mongoDB_URL + "/" + mongoDB_database_name;
+console.log(mongourl);
+//mongoose.connect(mongourl, { useNewUrlParser: true });
+mongoose.connect(mongourl, { useUnifiedTopology: true });
+
 //mongoose.connect('mongodb+srv://localhost:27017/test', { useNewUrlParser: true });
 //var db = mongoose.connection;
 
--- src/routes/data-management.js
+++ src/routes/data-management.js
@@ -12,6 +12,20 @@ router.get("/", (req, res, next) => {
 router.get("/populate", DataManagementController.populate);
 router.get("/clear",  DataManagementController.clear);
 
+mongoDB_URL                 = process.env.MONGODB_URL;
+mongoDB_database_user       = process.env.MONGODB_DATABASE-USER;
+mongoDB_database_password   = process.env.MONGODB_DATABASE_PASSWORD
+mongoDB_database_name       = process.env.MONGODB_DATABASE_NAME
+const mongourl = "mongodb://" + mongoDB_database_user + ":" + mongoDB_database_password +"@" + mongoDB_URL + "/" + mongoDB_database_name;
+
+router.get("/connect", (req, res, next) => {
+    return res.status(200).send({
+        message: mongourl
+    });
+});
+
+//console.log(mongourl);
+
 //router.delete("/clear", DataManagementController.clear);
 
 module.exports = router;
