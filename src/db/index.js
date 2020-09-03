151c66f4e923323a794da0f820a5501beee1784a
Mehmet Vural <mehmetalivural@yahoo.com>
2020-09-03 12:52:28 -0700

    test

--- src/db/index.js
+++ src/db/index.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 //const environment = require('../config/environment');
 mongoDB_URL                 = "172.30.229.9:27017";
-mongoDB_database_user       = process.env.MONGODB_USER;
-mongoDB_database_password   = process.env.MONGODB_PASSWORD
-mongoDB_database_name       = process.env.MONGODB_DATABASE
-const mongourl = "mongodb://" + mongoDB_database_user + ":" + mongoDB_database_password +"@" + mongoDB_URL + "/" + mongoDB_database_name;
-console.log(mongourl);
+mongoDB_user       = process.env.MONGODB_USER;
+mongoDB_password   = process.env.MONGODB_PASSWORD
+mongoDB_name       = process.env.MONGODB_DATABASE
+const mongourl = "mongodb://" + mongoDB_user + ":" + mongoDB_password +"@" + mongoDB_URL + "/" + mongoDB_name;
+//console.log(mongourl);
 //mongoose.connect(mongourl, { useNewUrlParser: true });
 mongoose.connect(mongourl, { useUnifiedTopology: true });
 
