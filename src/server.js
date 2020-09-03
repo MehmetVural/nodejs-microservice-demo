44c5b6aae0227c3dbdc70b35acbbe0ab082d816a
Mehmet Vural <mehmetalivural@yahoo.com>
2020-09-03 13:06:26 -0700

    keycloak

--- src/server.js
+++ src/server.js
@@ -14,61 +14,39 @@ app.use(cors());
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
   ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
-//mongoURLLabel = "";
+const router = express.Router()
 
 ///////////// KEYCLOAK
-// const express = require("express");
-var Keycloak = require('keycloak-connect');
-var session = require('express-session');
-//var memoryStore = new session.MemoryStore();
-
-const router = express.Router();
-
-//app.use(session({
-//    secret: '8d67fc9c-ddde-4b49-a78d-466279a10fdb',
-//    resave: false,
-//    saveUninitialized: true,
-//    store: memoryStore
-//}));
-
-//var keycloak = new Keycloak({
-//    store: memoryStore
-//});
-
-//app.use(keycloak.middleware({
-//    logout: '/logout',
-//    admin: '/admin'
-//}));
-//////
-
-
 // Provide the session store to the Keycloak so that sessions
 // can be invalidated from the Keycloak console callback.
 //
 // Additional configuration is read from keycloak.json file
 // installed from the Keycloak web console.
+// const express = require("express");
+var Keycloak = require('keycloak-connect');
+var session = require('express-session');
+var memoryStore = new session.MemoryStore();
+app.use(session({
+    secret: '8d67fc9c-ddde-4b49-a78d-466279a10fdb',
+    resave: false,
+    saveUninitialized: true,
+    store: memoryStore
+}));
+
+var keycloak = new Keycloak({
+    store: memoryStore
+});
+
+app.use(keycloak.middleware({
+    logout: '/logout',
+    admin: '/admin'
+}));
+// keycloak.protect('realm:admin'),
+//// END KEYCLOAK
+
 
 // APIS
-// keycloak.protect('realm:admin'),
 app.use("/workbc/api/v1", require("./routes"));
-
-/* Samples 
-app.get('/demo/public', function (req, res) {
-  res.json({ message: 'public' });
-});
-
-app.get('/demo/secured', keycloak.protect('realm:user'), function (req, res) {
-  res.json({ message: 'secured' });
-});
-
-app.get('/demo/admin', keycloak.protect('realm:admin'), function (req, res) {
-  res.json({ message: 'admin' });
-});
-
-//app.use('*', function (req, res) {
-//  res.send('Not found!');
-//});
-*/
 
 app.get('/', function (req, res) {
   res.send("Server is Running");
