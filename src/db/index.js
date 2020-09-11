453fc3d1977aaa0cc85f157cfb77debaf8edf7bd
Mehmet Vural <mehmetalivural@yahoo.com>
2020-09-10 18:24:47 -0700

    test

--- src/db/index.js
+++ src/db/index.js
@@ -5,15 +5,15 @@ mongoDB_password   = process.env.MONGODB_PASSWORD
 mongoDB_name       = process.env.MONGODB_DATBASE
 
 // Openshift config
-mongoDB_URL                 = "172.30.229.9:27017";
+mongoDB_URL                 = "172.30.34.74:27017";
 const mongourl = "mongodb://" + mongoDB_user + ":" + mongoDB_password +"@" + mongoDB_URL + "/" + mongoDB_name;
 // Local Debug Config
 //mongoDB_URL                 = "127.0.0.1:27017"; //DEV
 //const mongourl = "mongodb://" + mongoDB_URL + "/" + mongoDB_name; //DEV
 
 //console.log(mongourl);
-//mongoose.connect(mongourl, { useNewUrlParser: true });
 mongoose.connect(mongourl, { useNewUrlParser: true });
+//mongoose.connect(mongourl, {  useUnifiedTopology: true });
 
 //mongoose.connect('mongodb+srv://localhost:27017/test', { useNewUrlParser: true });
 //var db = mongoose.connection;
