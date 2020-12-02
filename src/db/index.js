0faa7af55a69eb193f90c3d74ac4a49a9058fe3e
Mehmet Vural <mehmetalivural@yahoo.com>
2020-12-02 09:37:30 -0800

    connecting db

--- src/db/index.js
+++ src/db/index.js
@@ -7,10 +7,14 @@ mongoDB_name       = process.env.MONGODB_DATBASE
 // Openshift config
 //mongoDB_URL                 = "172.30.34.74:27017";
 mongoDB_user                  = "user"
-mongoDB_user                  = "password"
+mongoDB_password              = "password"
 mongoDB_name                  = "mongodb"; 
 mongoDB_URL                   = "172.25.111.98:27017";
+//mongoDB_URL                   = "127.0.0.1:27017";
 const mongourl = "mongodb://" + mongoDB_user + ":" + mongoDB_password +"@" + mongoDB_URL + "/" + mongoDB_name;
+//const mongourl = "mongodb://172.25.111.98:27017/" + mongoDB_name;
+//const mongourl = "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false"
+
 // Local Debug Config
 //mongoDB_URL                 = "127.0.0.1:27017"; //DEV
 //mongoDB_URL                   = "172.25.111.98:27017"; //PROD
